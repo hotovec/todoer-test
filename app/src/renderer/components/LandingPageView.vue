edee79b894aaedac2727577b225815abf457a498
Michal Hotovec <michal.hotovec@gmail.com>
2017-03-30 22:58:16 +0200

    initial

---
+++ app/src/renderer/components/LandingPageView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <img src="./LandingPageView/assets/logo.png" alt="electron-vue">
+        <h1>Welcome.</h1>
+        <hr>
+        <current-page></current-page>
+        <versions></versions>
+        <links></links>
+        <hr>
+        <navigator></navigator>
+        <hr>
+    </div>
+</template>
+
+<script>
+  import CurrentPage from './LandingPageView/CurrentPage'
+  import Links from './LandingPageView/Links'
+  import Versions from './LandingPageView/Versions'
+  import Navigator from './Shared/Navigator'
+
+  export default {
+    components: {
+      Navigator,
+      CurrentPage,
+      Links,
+      Versions
+    },
+    name: 'landing-page'
+  }
+</script>
+
+<style scoped lang="scss">
+    img {
+        margin-top: -25px;
+        width: 450px;
+    }
+</style>
---
+++ app/src/renderer/components/Shared/Navigator.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="m-navigator">
+        NAVI: {{ title }}
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'navigator',
+
+    data () {
+      return {
+        title: 'This is navigator'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    code {
+        background-color: rgba(40, 56, 76, .5);
+        border-radius: 3px;
+        color: #ffffff;
+        font-weight: bold;
+        padding: 3px 6px;
+        margin: 0 3px;
+        vertical-align: bottom;
+    }
+
+    p {
+        line-height: 24px;
+    }
+</style>
