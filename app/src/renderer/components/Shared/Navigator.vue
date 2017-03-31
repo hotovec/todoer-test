f8fb174a0daf76ec0eb1156b457e31eaf58b43b9
Michal Hotovec <michal.hotovec@gmail.com>
2017-03-31 15:12:48 +0200

    element UI prehozene do "vnitrniho node_modules" + zkousky s JSX kompilaci (not working)

--- app/src/renderer/components/Shared/Navigator.vue
+++ app/src/renderer/components/Shared/Navigator.vue
@@ -1,16 +1,41 @@
 <template>
     <div class="m-navigator">
+        <div>
+            <i class="el-icon-edit"></i>
+            <i class="el-icon-share"></i>
+            <i class="el-icon-delete"></i>
+        </div>
+
+        <el-row gutter="20">
+            <el-col span="6">Col</el-col>
+            <el-col span="6">Col</el-col>
+            <el-col span="6">Col</el-col>
+            <el-col span="6">Col</el-col>
+        </el-row>
+
         NAVI: {{ title }}
+
+        <el-button type="primary" size="small">MyButton</el-button>
+
     </div>
 </template>
 
 <script>
+  import ElButton from 'element-ui'
+  import ElCol from 'element-ui/packages/col/src/col'
+  import ElRow from '../../../../node_modules/element-ui/packages/row/src/row'
   export default {
+    components: {
+      ElRow,
+      ElCol,
+      ElButton
+    },
     name: 'navigator',
 
     data () {
       return {
-        title: 'This is navigator'
+        title: 'This is navigator',
+        autoCommit: true
       }
     }
   }
